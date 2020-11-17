01f359cb65fc79001ec18930332d8098d896c87b
nn <Blubbel@Bartkauz.fritz.box>
2020-11-17 22:01:25 +0100

    initial G3

---
+++ cert_set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="cert_set" tilewidth="32" tileheight="32" tilecount="120" columns="8">
+ <image source="../blubbelcert/cert_set.png" width="256" height="480"/>
+</tileset>
