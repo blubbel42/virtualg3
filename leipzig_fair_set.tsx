7b5cdfb7129cac9863890e3bf68aca387c2d4e96
nn <Blubbel@Bartkauz.fritz.box>
2020-11-17 23:53:19 +0100

    clean up

--- leipzig_fair_set.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="leipzig_fair_set" tilewidth="32" tileheight="32" tilecount="32" columns="8">
- <image source="../blubbelcert/leipzig_fair_set.png" width="256" height="128"/>
-</tileset>
